8291007a7734db5b9d5ca2644fb09f0c721b12b0
Matthieu Beauvin <matthieu.beauvin@gmail.com>
2024-02-18 12:15:59 +0100

    First commit to share with my mac

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Routing } from './utils/routing';
+import React from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View> */}
+      <Routing />
+    </>
   );
 }
 
---
+++ screens/home.tsx
@@ -0,0 +1,41 @@
+import { Link } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack/lib/typescript/src/types';
+import React from 'react';
+import { Alert, Button } from 'react-native';
+import { SignUpScreen } from './sign-up';
+import { Routes } from '../utils/routing';
+
+type RootStackParamList = {
+    Home: JSX.Element;
+};
+
+export type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>
+
+type Props = {
+    navigation: HomeScreenProps;
+};
+
+export const HomeScreen = ({ navigation }: Props) => {
+    const createTwoButtonAlert = () =>
+        Alert.alert('Alert Title', 'My Alert Msg', [
+            {
+                text: 'Cancel',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+            },
+            { text: 'OK', onPress: () => console.log('OK Pressed') },
+        ]);
+
+    console.log("navigation:", navigation)
+
+    return (
+        <>
+            <Link to={Routes.SIGN_UP.path}>Créer un compte</Link>
+            <Link to={Routes.LOGGED_OUT_SCAN_BOARD.path}>Commencer sans compte</Link>
+            <Button
+                title="Go to Jane's profile"
+                onPress={createTwoButtonAlert}
+            />
+        </>
+    );
+};
